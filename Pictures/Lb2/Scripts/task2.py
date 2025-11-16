f5c15eb10972caf2bf17a9c505ab7e8c27cda80e
sofiiashmatko <sofiia.shmatko@nure.ua>
2025-11-16 13:34:20 +0200

    Add files via upload

---
+++ Pictures/Lb2/Scripts/task2.py
@@ -0,0 +1,10 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/hello", methods=["GET"])
+def hello():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run(port=8000)
